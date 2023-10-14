3e6e4184969f1f4a4ff79f7823f126b3048f88aa
Azusa Fukuda <afukuda@mySeneca.ca>
2023-10-14 14:39:54 -0400

    Added regestrations w name

---
+++ project/models/fct/fct_toronto_library_ragestrations_w_name.sql
@@ -0,0 +1,23 @@
+WITH
+r AS (
+   SELECT
+      * 
+   FROM
+      {{ref('src_toronto_library_registrations')}}
+),
+i AS (
+   SELECT
+      *
+   FROM
+      {{ref('src_toronto_library_info')}}
+)
+
+SELECT
+   r._id,
+   r.year,
+   r.branch_code,
+   i.branch_name,
+   r.registrations,
+   AVG(registrations) OVER() AS avg
+FROM r 
+LEFT JOIN i ON (i.branch_code = r.branch_code)

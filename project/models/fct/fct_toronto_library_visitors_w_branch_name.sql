5e105e1bcb955f9cab8b555879842ec718ae9fab
Azusa Fukuda <afukuda@mySeneca.ca>
2023-10-14 14:47:28 -0400

    Added visitors with branch name

---
+++ project/models/fct/fct_toronto_library_visitors_w_branch_name.sql
@@ -0,0 +1,23 @@
+WITH
+v AS (
+   SELECT
+      * 
+   FROM
+      {{ref('src_toronto_library_visitors')}}
+),
+i AS (
+   SELECT
+      *
+   FROM
+      {{ref('src_toronto_library_info')}}
+)
+
+SELECT
+   v._id,
+   v.year,
+   v.branch_code,
+   i.branch_name,
+   v.visits,
+   AVG(visits) OVER() AS avg
+FROM v 
+LEFT JOIN i ON (i.branch_code = v.branch_code)
